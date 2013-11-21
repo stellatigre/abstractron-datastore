6c855f4df2bc2b1e359495abc8dcc2b9ccf4a473
Chris Mahoney <chris.p.mahoney@gmail.com>
2013-11-21 03:46:04 -0600

    * Added facebook oAuth2 process (nodehost/auth/facebook via GET, must manually respond on page). Anyone authenticating against that URL will have their profile serialized into abstractapi.db.sessions collection in mongo. * Added MongoStore to auto-handle session negotiation. * General cleanup of old user testing crap * Added video route

--- app.js
+++ app.js
@@ -2,27 +2,28 @@
 var flash 		= require('connect-flash'),	// flash
 	express  	= require('express'),	// express
 	mongo		= require('mongodb'),
+	MongoStore  = require('connect-mongo')(express), // mongo auth store
 	mongoose 	= require('mongoose'),	// mongoose
 	Schema  	= mongoose.Schema,
 	passport	= require('passport'),	// passport
 	LocalStrategy = require('passport-local').Strategy,
+	// LocalStrategy: basic auth
 	
+	FacebookStrategy = require('passport-facebook').Strategy,
+	// App ID/Secret
+	FACEBOOK_APP_ID = "1436345559910277",
+	FACEBOOK_APP_SECRET = "54b38ae41d350af4e506b1218feb17ee",
+
 	app      	= express(),	
 	
 	// route modules
     users = require('./routes/users'),
 	images = require('./routes/images'),
+	videos = require('./routes/videos'),
 	levels = require('./routes/levels'),
 	blocks = require('./routes/blocks'),
 	login  = require('./routes/login'),
 	register = require('./routes/register');
-
-// Test user records
-var userList = [
-	{ _id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' },
-	{ _id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' },
-	{ _id: 3, username: 'chris', password: 'chris', email: 'chris@abstractron.com' }
-];	
 
 // User methods
 function findUserById(id, fn) {
@@ -31,40 +32,59 @@ function findUserById(id, fn) {
     BSON = mongo.BSONPure;
 
 	var server = new Server('localhost', 27017, {auto_reconnect: true});
-	db = new Db('abstractapi', server);
+	db = new Db('abstractapi', server, {safe: false});
 	
 	db.open(function (err, db) {
 		if (err) {
-			//sys.puts(err);
+			fn(new Error('Err: ' + err));
 		} else {
+			// local
 			db.collection('users', function (err, collection) {
-				collection.findOne({_id: id }, function (err, item) {
-					console.log('FindById: ' + item);
+				collection.findOne({_id: new BSON.ObjectID(id) }, function (err, item) {
 					return fn(null, item);
 				});
 			});
 		}
 	});	
+	fn(new Error('User ' + id + ' does not exist'));
+}
+
+function findSessionUserById(id, fn) {
+	var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+	var server = new Server('localhost', 27017, {auto_reconnect: true});
+	db = new Db('abstractapi', server, {safe: false});
 	
+	db.open(function (err, db) {
+		if (err) {
+			fn(new Error('Err: ' + err));
+		} else {
+			// facebook
+			db.collection('sessions', function (err, collection) {
+				collection.findOne({_id: new BSON.ObjectID(id) }, function (err, item) {
+					return fn(null, item);
+				});
+			});
+		}
+	});	
+
 	fn(new Error('User ' + id + ' does not exist'));
 }
 
 function findUserByUsername(username, fn) {
-	
-	// Return joe only
-	//var user = userList[1];
-	//return fn(null, user);
 	
 	var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
 	var server = new Server('localhost', 27017, {auto_reconnect: true});
-	db = new Db('abstractapi', server);
+	db = new Db('abstractapi', server, {safe: false});
 	
 	db.open(function (err, db) {
 		if (err) {
-			//sys.puts(err);
+			return fn(new Error('Err: ' + err));
 		} else {
 			db.collection('users', function (err, collection) {
 				collection.findOne({username: username }, function (err, item) {
@@ -74,21 +94,7 @@ function findUserByUsername(username, fn) {
 			});
 		}
 	});	
-	
-	// Bad login (crashes)
-	//return fn(null, null);
 }
-
-/* PASSPORT USER SERIALIZATION */
-passport.serializeUser(function(user, done) {
-	done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-	findUserById(id, function(err, user) {
-		done(err, user);
-	});
-});
 
 /* PASSPORT - LOCAL STRATEGY */
 passport.use(new LocalStrategy(
@@ -115,29 +121,71 @@ passport.use(new LocalStrategy(
 		});
 	}
 ));
+
+/* PASSPORT - FACEBOOK */
+passport.use(new FacebookStrategy({
+	clientID: FACEBOOK_APP_ID,
+	clientSecret: FACEBOOK_APP_SECRET,
+	callbackURL: "http://devnode.vmception.com:3002/auth/facebook/callback"
+	},
+	function(accessToken, refreshToken, profile, done) {
+		console.log(profile);
+		
+		
+		return done(null, profile);
+	}
+));
+
+/* PASSPORT USER SERIALIZATION */
+passport.serializeUser(function(user, done) {
+	console.log("SESSION: Serialize user...");
 	
-/* PASSPORT - ENSURE AUTH */
-/*
-function ensureAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) { return next(); }
-	res.redirect('/login');
-}
-*/
+	
+
+	done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+	console.log("SESSION: Deserialize user (" + id + ")");
+	
+	findSessionUserById(id, function(err, obj) {
+		console.log('findSessionUserById (' + id + ': found: ' + obj);
+		
+		done(err, obj);
+	});
+});
 	
 /* SERVER CONFIG */
 app.configure(function () {
 	app.use(express.logger());
 	app.use(express.cookieParser('abstractsecret'));
 	app.use(express.bodyParser());
-	//app.use(express.session({ cookie: { maxAge: 60000 }}));
+	app.use(express.methodOverride());
+	app.use(express.session({ 
+		secret: 'abstractsecret', 
+		store: new  MongoStore({
+			db: 'abstractapi',
+			collection: 'sessions',
+			
+		}, function() {
+			console.log('MongoStore session db connection OK.');
+		})
+	}));
 	app.use(flash());
 	// Initialize passport and use sessions
 	app.use(passport.initialize());
 	app.use(passport.session());
-	//app.use(app.router);
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 /* ROUTES */
+/*
+// Test route: /restricted
+app.get('/restricted', ensureAuthenticated, function(req, res) {
+	// TODO: some response for authed users
+});
+*/
 
 // /login
 
@@ -150,12 +198,34 @@ app.post('/login',
 );
 
 // /logout
-/*
 app.get('/logout', function(req, res) {
 	req.logout();
 	res.redirect('/');
 });
+
+/* ENSURE AUTH 
+	Use this route on any protected resource, will be allowed
+	if authenticated otherwise redirect to login.
 */
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) { return next(); }
+	res.redirect('/login');
+}
+
+// /auth
+
+/// facebook
+app.get('/auth/facebook',
+	passport.authenticate('facebook'),
+	function(req, res) {
+		// Request will redirect to FB for auth, this will not be hit
+	});
+	
+app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', { failureRedirect: '/login' }),
+	function (req, res) {
+		res.send('Logged in');
+	});
 
 app.post('/authfail', login.loginFailed);
 app.post('/authok', login.loggedIn);
@@ -174,6 +244,13 @@ app.get('/images/:id', images.findById);
 app.post('/images', images.addImage);
 app.put('/images/:id', images.updateImage);
 app.delete('/images/:id', images.deleteImage);
+
+// /videos
+app.get('/videos', videos.findAll);
+app.get('/videos/:id', videos.findById);
+app.post('/videos', videos.addVideo);
+app.put('/videos/:id', videos.updateVideo);
+app.delete('/videos/:id', videos.deleteVideo);
 
 // /levels
 app.get('/user/:userid/levels', levels.findByUserId);
