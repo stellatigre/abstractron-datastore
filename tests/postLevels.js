2104d9bf622a0c5da438a6120829b774ea49ab95
Stella Cannefax <stella.cannefax@gmail.com>
2013-12-26 13:38:57 -0600

    ALL OF THIS COMMIT IS BROKEN ... at the moment. Commiting to this branch for stash work I've done already. DO NOT MERGE THIS OMFG

--- tests/postLevels.js
+++ tests/postLevels.js
@@ -4,8 +4,7 @@ var assert = require("chai").assert;
 
 var AT = require('./lib/testHelpers.js');		// plz do not change directory structure
 var conf = require('./lib/testConfig.json');  	
-var goodTestLevel = require('./lib/levelTest.json')[0];
-var badTestLevel = require('./lib/badLevelTest.json')[0];
+var goodTestLevel = require('./lib/OneGoodLevel.json');
 
 describe ("POST /levels - ", function () {
 
@@ -26,28 +25,20 @@ describe ("POST /levels - ", function () {
             	if (err) done(err);                         // test after we save it
            		responseData = JSON.parse(body);    // save it here
 				assert.equal(res.statusCode, 200);  // Make sure we get a 200 OK
+				
+				console.log(responseData);
         	});
         	waitForResponseData(done);		// don't test until we get the response
 	});
 	
-
 	it('should return a new Object "_id", a string', function (done) {	
 		assert.isString(responseData['_id']);	// we get an Object back ?	
 		done();
 	});
 
-	it('should produce a response that has "name" & "rooms" fields which match our input', function(done) { 
-	
-		assert.equal(responseData['name'], goodTestLevel['name']);
-		assert.equal(responseData['rooms'] ,goodTestLevel['rooms']);
-	
-		done();
-	});		
-
-
 	it('should return an informative error & a 400 status code if the name is missing', function (done) {	
 
-		var badNameLevel = badTestLevel;
+		var badNameLevel = goodTestLevel;
 		badNameLevel.name = "";
 	
 		req({
@@ -65,18 +56,16 @@ describe ("POST /levels - ", function () {
 		);
 	});
 	
-	it('should return an informative error & 400 status code if the block data isn\'t numbers', function (done) {	
+	it('inserted object should then be retrievable via GET /levels/:id', function (done) {	
 
 		req({
-			uri: conf.baseUrl+path,
-			method : "POST",
-			form : badTestLevel
+			uri: conf.baseUrl+path+'/'+responseData._id,
+			method : "GET"
 		},	function(err, res, body) {
 				if (err) done(err);
 				var data = JSON.parse(body);
 				
-				assert.equal (res.statusCode , 400);
-				assert.equal('URL value appears to not be valid.', data['error']);		
+				assert.equal(responseData._id , data._id);		
 				done();
 			}
 		);
